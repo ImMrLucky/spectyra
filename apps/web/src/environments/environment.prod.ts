1fa650ff44c81038eb11e55a47d40e1e226e8d51
kent hamilton <uidevkent@gmail.com>
2026-01-24 22:01:29 -0500

    updated app base api url

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // Use relative URL - Netlify will proxy /v1/* to your API via netlify.toml redirects
-  // Update the redirect in netlify.toml to point to your Railway API URL
-  apiUrl: '/v1',
+  apiUrl: 'https://spectyra.up.railway.app/v1',
 };
